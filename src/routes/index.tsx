81c043ad13ef1388f05006333d6d493f5bebb06f
Bhupendra Bhatta <bhupendra.bhatta@outcodesoftware.com>
2026-01-30 09:00:32 +0545

    Added shadcn components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,32 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { Table, TableBody, TableHeader } from '@/components/ui/table'
+import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute("/")({ component: App });
+export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       <div className="font-medium">Hello World</div>
+      <div>
+        <Table>
+          <TableHeader>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+            </tr>
+          </TableHeader>
+          <TableBody>
+            <tr>
+              <td>Alice</td>
+              <td>30</td>
+            </tr>
+            <tr>
+              <td>Bob</td>
+              <td>25</td>
+            </tr>
+          </TableBody>
+        </Table>
+      </div>
     </div>
-  );
+  )
 }
